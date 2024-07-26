3719aaf0dfd0579a88dfb4c3a2a5b5f8bf8a8114
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-25 22:26:29 -0400

    build: update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 
 export default createConfigForNuxt({
@@ -28,7 +27,31 @@ export default createConfigForNuxt({
       "@stylistic/semi": ["error", "always"],
       "@stylistic/comma-dangle": ["error", "always-multiline"],
       "@stylistic/indent": ["error", 2],
-      "import/order": ["error", { "newlines-between": "always", alphabetize: { order: "asc" } }],
+      "@stylistic/quote-props": ["error", "consistent-as-needed"],
+      "@stylistic/arrow-parens": ["error", "always"],
+      "@stylistic/operator-linebreak": ["error", "before"],
+      "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: false }],
+      "@stylistic/member-delimiter-style": [
+        "error",
+        {
+          multiline: {
+            delimiter: "semi",
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: "semi",
+            requireLast: false,
+          },
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          alphabetize: { order: "asc" },
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        },
+      ],
       "vue/max-attributes-per-line": [
         "error",
         {
@@ -64,6 +87,13 @@ export default createConfigForNuxt({
           ],
         },
       ],
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/html-closing-bracket-newline": "off",
+      "vue/mustache-interpolation-spacing": "off",
+      "vue/require-v-for-key": "error",
+      "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "jsdoc/check-param-names": "off",
     },
   }
 );
